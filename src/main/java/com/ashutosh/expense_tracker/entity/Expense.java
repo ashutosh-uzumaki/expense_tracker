1d736d70775cee73cc948cf938703d18506fad2b
ashutosh-uzumaki <ashutosh.dev18@gmail.com>
2024-10-19 20:16:18 +0530

    expense tracker

--- src/main/java/com/ashutosh/expense_tracker/entity/Expense.java
+++ src/main/java/com/ashutosh/expense_tracker/entity/Expense.java
@@ -1,14 +1,15 @@
 package com.ashutosh.expense_tracker.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.*;
 
 import java.math.BigDecimal;
 import java.sql.Date;
@@ -37,4 +38,10 @@ public class Expense {
     private Timestamp created_at;
     @UpdateTimestamp
     private Timestamp updated_at;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name="user_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE) // delete the expenses when the user is deleted.
+    @JsonIgnore //not to fetch the user while fetching the expense.
+    private User user;
 }
